68cfdeb471daa1b18b673ae78e162062f0cfa390
AislanMunin <aislan.munin@gmail.com>
2024-08-30 23:16:49 -0300

    3

---
+++ utils/_banner.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+  clear
+
+
+printf "${GREEN}";
+printf "Clickr Marketing de Resultados - ClcikrChat\n";
+
+printf "\n"
+
+printf "2024 @ Todos os direitos reservados a https://ClickrChat.com\n"
+
+
+
+  printf "${NC}";
+
+  printf "\n"
+}
